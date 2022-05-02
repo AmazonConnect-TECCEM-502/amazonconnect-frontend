898f8603106258105ac05fbc08971ab7c85f8859
a01750264 <a01750246@itesm.mx>
2022-05-02 16:54:17 -0500

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import ListaCategoriasProblemas from "./components/ListaCategoriasProblemas";
 import NavBar from "./components/NavBar/NavBar";
-import Tarjeta from "./components/Tarjeta";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Tarjeta componente={<ListaCategoriasProblemas />} />
     </div>
   );
 }
