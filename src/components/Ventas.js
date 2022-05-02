c24c6d08d58cc531b88514e335531ad51c64c0a7
OmarSorchini <omarsorchinijr@gmail.com>
2022-05-02 18:01:50 -0500

    added css for sales and basic component functionality

--- src/components/Ventas.js
+++ src/components/Ventas.js
@@ -1,11 +1,25 @@
 import { Fragment } from "react";
-import "../style/Ventas.css"
+import Tarjeta from "./Tarjeta";
+
+const productos = [{title: "producto1",
+                    desc: "la desc"},
+                    {title: "producto2",
+                    desc: "la desc"},
+                    {title: "producto3",
+                    desc: "la desc"},
+                ];
 
 const Ventas = (props) => {
-    return(
+  return (
+    <Tarjeta
+      componente={productos.map((producto) => (
         <Fragment>
+          <h1>{producto.title}</h1>
+          <p>{producto.desc}</p>
         </Fragment>
-    );
+      ))}
+    />
+  );
 };
 
 export default Ventas;
