6c92e5e427853f5f074c3940729f81415f3095e1
OmarSorchini <omarsorchinijr@gmail.com>
2022-05-04 22:16:43 -0500

    sales component works and cards are now displayed correctly

--- src/App.js
+++ src/App.js
@@ -14,19 +14,21 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Card component= {<AnswerList/>} />
-      <Card component = {<QuestionList/>}/>
-      <Card component = {<ProblemCategoryList/>}/>
-      <Card component = {<Menu/>}/>
-      <Card component = {<UserCard  image = "TelmexLogo" 
-                                    fname = "John" 
-                                    lname = "Doe"
-                                    email = "example@gmail.com"
-                                    phone = "55 5555 5555" />}/>
-      <Card component = {<ProductCard image = "ejemploTelmex"
-                                      name = "Internet Paquete"
-                                      price = "56"
-                                      desc = "bla bla"  />}/>
+      <div>
+        <Card component= {<AnswerList/>} />
+        <Card component = {<QuestionList/>}/>
+        <Card component = {<ProblemCategoryList/>}/>
+        <Card component = {<Menu/>}/>
+        <Card component = {<UserCard  image = "TelmexLogo" 
+                                      fname = "John" 
+                                      lname = "Doe"
+                                      email = "example@gmail.com"
+                                      phone = "55 5555 5555" />}/>
+        <Card component = {<ProductCard image = "ejemploTelmex"
+                                        name = "Internet Paquete"
+                                        price = "56"
+                                      desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie dui quam. Aenean egestas volutpat mauris non tincidunt. Pellentesque maximus a enim sed mollis. Suspendisse lacus risus, cursus a blandit sit amet, sollicitudin vitae quam. Morbi vulputate venenatis ipsum id auctor. Sed sodales nulla sapien, sit amet interdum lorem mollis vitae. In ut sem turpis. Nulla faucibus, ante eget rhoncus venenatis, ipsum massa congue purus, fringilla elementum purus purus et nunc. Curabitur id risus sed lorem viverra semper vel id metus. Praesent vitae bibendum augue."  />}/>
+      </div>
     </div>
   );
 }
