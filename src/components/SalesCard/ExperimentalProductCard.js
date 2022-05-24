a73d679f7348d069d4c0d8cab5bb62dc96c30759
A01750400 <a01750400@tec.mx>
2022-05-24 18:08:21 -0500

    experimental sales card

---
+++ src/components/SalesCard/ExperimentalProductCard.js
@@ -0,0 +1,61 @@
+import { Fragment } from "react";
+import ProductImage from './ProductImage';
+import ProductName from './ProductName';
+import ProductPrice from './ProductPrice';
+import ProductDescription from './ProductDescription';
+import { useState } from "react";
+
+
+const ProductCard = (props) => {
+
+  const [productKey, setKey] = useState('TV');
+  const [productsArr, setProducts] = useState(props.products['internet']);
+
+  const NextButton = (event) => {
+    if (productKey === 'internet') {
+      setKey('TV');
+    }
+    else if (productKey === 'TV') {
+      setKey('mobile');
+    }
+    else if (productKey === 'mobile') {
+      setKey('internet');
+    }
+    //console.log(productKey);
+    setProducts(props.products[productKey]);
+  };
+
+  return(
+    <div className="product-card">
+        <div className="product-header">
+          <img src = {require(`../../images/sales/${productsArr.image}.jpg`)} alt = "Product" />
+          <div>
+            <p className="product-name">{productsArr.name}</p>
+            <p className="product-price">Price: {productsArr.price} $</p>
+            <button className="add-button">Add</button>
+            <button onClick={NextButton}>{productKey}</button>
+          </div>
+        </div>
+        <p className="product-description"> {productsArr.desc} </p>
+
+    </div>
+  )
+    /*return(
+
+        <div className="product-card">
+            <Fragment>
+              <div>
+                <img src={props.img} />
+                <ProductImage image = {props.image} />
+                <ProductName name = {props.name} />
+                <ProductPrice price = {props.price} />
+              </div>
+              <ProductDescription desc = {props.desc} />
+
+            </Fragment>
+        </div>
+
+    );*/
+};
+
+export default ProductCard;
