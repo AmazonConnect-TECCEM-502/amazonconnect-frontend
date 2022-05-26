932d8f3d9f97d515c747a8d5b1a939f4bacae709
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-05-25 19:17:06 -0500

    Update ManagerAgentsDashboard view and  ManagerMain.js, create Agent, Agents

---
+++ src/components/ManagerCards/Agent.js
@@ -0,0 +1,22 @@
+/*
+  Author: Maria Fernanda Ramirez Barragan
+  Description: Gets the agent name and gives 
+  an specific style
+
+  Usage:
+  <Agent />
+*/
+
+import { Fragment } from "react";
+
+const Agent = () => {
+  return (
+    <Fragment>
+      <div className="agent-info">
+        <p className="agent-name"> Agent </p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Agent;
