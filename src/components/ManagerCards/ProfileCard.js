ba484cc2beb53eb1bda17f812a90617bf6f435b7
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-05-26 16:33:36 -0500

    Update ProfileCard.js

--- src/components/ManagerCards/ProfileCard.js
+++ src/components/ManagerCards/ProfileCard.js
@@ -13,9 +13,9 @@ const ProfileCard = () => {
   return (
     <Fragment>
       <div className="profile-info">
-        <div>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <img
-            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580"
             alt="Profile Photo"
           />
         </div>
