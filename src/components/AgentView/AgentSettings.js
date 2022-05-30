e6a53f60b6aa841895201104c1c983f9bffbe354
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-05-30 16:50:25 -0500

    Update ManagerSettings and AgentSettings

--- src/components/AgentView/AgentSettings.js
+++ src/components/AgentView/AgentSettings.js
@@ -14,16 +14,21 @@ import Settings from "../AgentCards/Setting";
 import NavBar from "../NavBar/NavBar";
 
 function AgentSettings() {
-    return (
-      <div>
+  return (
+    <div>
       <CardsProvider>
-      <NavBar />
-        <AgentBoard>
-          <Card id="card-7" draggable="true" component={ <Settings />}/>
-        </AgentBoard>
+        <NavBar />
+        <div
+          className="manager-dashboard-container"
+          style={{ justifyContent: "space-around" }}
+        >
+          <AgentBoard id="board-4" className="board board-menu profile-card">
+            <Card id="card-7" draggable="true" component={<Settings />} />
+          </AgentBoard>
+        </div>
       </CardsProvider>
-      </div>
-    );
-  }
-  
-  export default AgentSettings;
+    </div>
+  );
+}
+
+export default AgentSettings;
