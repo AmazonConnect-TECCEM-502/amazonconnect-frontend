2a052b1f3280acd53bf3b7489a49dcef0713d509
Emilio Rivers <rivers.emilio2001@protonmail.com>
2022-05-31 14:02:34 -0500

    Amazon CCP directly embedded

--- src/components/AgentCards/AmazonCCP.js
+++ src/components/AgentCards/AmazonCCP.js
@@ -1,12 +1,31 @@
-import { Fragment } from "react";
+import { Component } from 'react';
+import React from 'react';
+import 'amazon-connect-streams';
 
-const AmazonCCP = () => {
-    return (
-        <Fragment>
-            <h1>Contact Control Panel</h1>
-            <iframe src="https://3.80.211.101:8443" width="400px" height="500px" title="EMBED" />
-        </Fragment>
-    );
-};
+class AmazonCCP extends Component {
+    constructor(props) {
+        super(props);
+        this.containerDiv = React.createRef();
+    }
+
+    componentDidMount() {
+        connect.core.initCCP(this.containerDiv.current, {
+            ccpUrl: 'https://tec2022bloqueaws.my.connect.aws/connect/ccp-v2/',
+            loginPopup: true,
+            softphone: {
+                allowFramedSoftphone: true
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="ccp">
+                <h1>Contact Control Panel</h1>
+                <div style={{width: "400px", height: "500px" }} className="containerDiv" ref={this.containerDiv} />
+            </div>
+        );
+    }
+}
 
 export default AmazonCCP;
