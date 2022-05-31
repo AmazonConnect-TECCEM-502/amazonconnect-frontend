df1d71e0805ffc38ebe9e761001b2f699769b8b3
A01750400 <a01750400@tec.mx>
2022-05-30 23:22:59 -0500

    Added navigation functionality to sales card

--- src/components/SalesCard/ProductsCategoryList.js
+++ src/components/SalesCard/ProductsCategoryList.js
@@ -1,9 +1,8 @@
 import SearchBar from "../AgentCards/SearchBar";
 import { Fragment, useEffect, useState } from "react";
 
-const ProductsCategoryList = () => {
+const ProductsCategoryList = (props) => {
   const [products, setProducts] = useState([]);
-  const [activeLink, setActiveLink] = useState(0);
 
   // useEffect will be used so before the component is created it fetch the data from our back
   useEffect(() => {
@@ -17,14 +16,6 @@ const ProductsCategoryList = () => {
     fetchData();
   }, []);
 
-  const displayAvailableProducts = async (id) => {
-    // this function will allow us to display the available products on another card
-    const data = {
-      category_id: id,
-    };
-    console.log(JSON.stringify(data), "Product");
-  };
-
   return (
     <Fragment>
       <p className="title"> Products by category</p>
@@ -34,12 +25,8 @@ const ProductsCategoryList = () => {
           <div className="categorys">
             <button
               type="text"
-              className={
-                activeLink === product.category_id && "categorys-active"
-              }
               onClick={() => {
-                setActiveLink(product.category_id);
-                displayAvailableProducts(product.category_id);
+                props.buttonAction(product.category_id);
               }}
             >
               {product.category_name}
