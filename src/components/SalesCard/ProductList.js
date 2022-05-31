1710cd7ddab79dc6f08f4a7569e377aa848c469b
A01750400 <a01750400@tec.mx>
2022-05-30 19:29:00 -0500

    get products through props

--- src/components/SalesCard/ProductList.js
+++ src/components/SalesCard/ProductList.js
@@ -1,13 +1,13 @@
 import SearchBar from "../AgentCards/SearchBar";
 import { Fragment, useEffect, useState } from "react";
 
-const ProductList = () => {
+const ProductList = (props) => {
   const [products, setProducts] = useState([]);
   
   useEffect(() => {
     const fetchData = async () => {
       //const productsData = await fetch("http://3.80.44.247:3000/sales/getRecommendedProducts/1/1");
-      const productsData = await fetch("http://localhost:8080/sales/getRecommendedProducts/1/1");
+      const productsData = await fetch(`http://localhost:8080/sales/getRecommendedProducts/${props.client_id}/${props.category_id}`);
       const jsonProducts = await productsData.json();
 
       setProducts(jsonProducts[0]);
