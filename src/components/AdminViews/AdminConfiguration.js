6c757492b243f19ad971d350d09ce546e6121ec2
JoanDGG <ggjoan2000@gmail.com>
2022-05-31 21:04:53 -0500

    Update AdminConfiguration.js

--- src/components/AdminViews/AdminConfiguration.js
+++ src/components/AdminViews/AdminConfiguration.js
@@ -1,7 +1,6 @@
 import AgentBoard from "../AgentCards/AgentBoard"
 import MenuConfiguration from "../Menu/MenuConfiguration"
 import NavBarAdmin from "../NavBar/NavBarAdmin";
-import NewQuestion from "../AdminCards/NewQuestion";
 import NewProduct from "../AdminCards/NewProduct";
 import NewUser from "../AdminCards/NewUser";
 import UpdateUser from "../AdminCards/UpdateUser";
@@ -14,10 +13,14 @@ import AdminCardProvider from "../AdminCards/AdminCardProvider";
 import NewAnswer from "../AdminCards/NewAnswer";
 import ClientForms from "../ClientCard/ClientForms";
 import AgentProvider from "../AgentView/AgentProvider";
+import AdminContextProvider from "../AdminCards/AdminContextProvider";
+import NewProblem from "../AdminCards/NewProblem";
+import ProposalsList from "../AdminCards/ProposalsList";
 
 function AdminConfiguration() {
   return (
     <AdminCardProvider>
+    <AdminContextProvider>
     <NavBarAdmin />
     <div className="agent-container admin">
       <AgentBoard id="board-1" className="board board-menu">
@@ -27,7 +30,7 @@ function AdminConfiguration() {
       </AgentBoard>
       
       <AgentBoard id="board-2" className="board board-menu">
-        <AdminCard id="card-4" draggable="true" component={<NewQuestion/>} />
+        <AdminCard id="card-4" draggable="true" component={<NewProblem/>} />
         <AdminCard id="card-6" draggable="true" component={<NewUser/>} />
         <AdminCard id="card-8" draggable="true" component={<NewProduct/>} />
       </AgentBoard>
@@ -38,8 +41,10 @@ function AdminConfiguration() {
         <AdminCard id="card-7" draggable="true" component={<UpdateUser/>} />
         <AdminCard id="card-12" draggable="true" component={<UpdateAnswerList />} />
         <AdminCard id="card-13" draggable="true" component={<NewAnswer/>}/>
+        <AdminCard id="card-14" draggable="true" component={<ProposalsList/>}/>
       </AgentBoard>
     </div>
+    </AdminContextProvider>
     </AdminCardProvider>
   );
 }
