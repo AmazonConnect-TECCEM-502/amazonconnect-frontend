edc1bd2bfd59ccb0109229411f869c99926ea1d8
a01750264 <a01750246@itesm.mx>
2022-06-02 18:05:55 -0500

    fixing merge

--- src/components/AdminViews/AdminConfiguration.js
+++ src/components/AdminViews/AdminConfiguration.js
@@ -21,77 +21,75 @@ function AdminConfiguration() {
   return (
     <AdminCardProvider>
       <AdminContextProvider>
-        <NavBarAdmin />
-        <div className="agent-container admin">
-          <AgentBoard id="board-1" className="board board-menu">
-            <AdminCard
-              id="card-1"
-              draggable="false"
-              component={<MenuConfiguration />}
-            />
-            <AdminCard
-              id="card-10"
-              draggable="true"
-              component={
-                <AgentProvider>
-                  <ClientForms />
-                </AgentProvider>
-              }
-            />
-            <AdminCard
-              id="card-11"
-              draggable="true"
-              component={<NewCategory />}
-            />
-          </AgentBoard>
+        <ClientProvider>
+          <NavBarAdmin />
+          <div className="agent-container admin">
+            <AgentBoard id="board-1" className="board board-menu">
+              <AdminCard
+                id="card-1"
+                draggable="false"
+                component={<MenuConfiguration />}
+              />
+              <AdminCard
+                id="card-10"
+                draggable="true"
+                component={<ClientForms />}
+              />
+              <AdminCard
+                id="card-11"
+                draggable="true"
+                component={<NewCategory />}
+              />
+            </AgentBoard>
 
-          <AgentBoard id="board-2" className="board board-menu">
-            <AdminCard
-              id="card-4"
-              draggable="true"
-              component={<NewProblem />}
-            />
-            <AdminCard id="card-6" draggable="true" component={<NewUser />} />
-            <AdminCard
-              id="card-8"
-              draggable="true"
-              component={<NewProduct />}
-            />
-          </AgentBoard>
+            <AgentBoard id="board-2" className="board board-menu">
+              <AdminCard
+                id="card-4"
+                draggable="true"
+                component={<NewProblem />}
+              />
+              <AdminCard id="card-6" draggable="true" component={<NewUser />} />
+              <AdminCard
+                id="card-8"
+                draggable="true"
+                component={<NewProduct />}
+              />
+            </AgentBoard>
 
-          <AgentBoard id="board-3" className="board board-menu">
-            <AdminCard
-              id="card-5"
-              draggable="true"
-              component={<UpdateQuestionList />}
-            />
-            <AdminCard
-              id="card-9"
-              draggable="true"
-              component={<UpdateProduct />}
-            />
-            <AdminCard
-              id="card-7"
-              draggable="true"
-              component={<UpdateUser />}
-            />
-            <AdminCard
-              id="card-12"
-              draggable="true"
-              component={<UpdateAnswerList />}
-            />
-            <AdminCard
-              id="card-13"
-              draggable="true"
-              component={<NewAnswer />}
-            />
-            <AdminCard
-              id="card-14"
-              draggable="true"
-              component={<ProposalsList />}
-            />
-          </AgentBoard>
-        </div>
+            <AgentBoard id="board-3" className="board board-menu">
+              <AdminCard
+                id="card-5"
+                draggable="true"
+                component={<AdminProblemList />}
+              />
+              <AdminCard
+                id="card-9"
+                draggable="true"
+                component={<UpdateProduct />}
+              />
+              <AdminCard
+                id="card-7"
+                draggable="true"
+                component={<UpdateUser />}
+              />
+              <AdminCard
+                id="card-12"
+                draggable="true"
+                component={<AdminSolutionList />}
+              />
+              <AdminCard
+                id="card-13"
+                draggable="true"
+                component={<NewSolution />}
+              />
+              <AdminCard
+                id="card-14"
+                draggable="true"
+                component={<ProposalsList />}
+              />
+            </AgentBoard>
+          </div>
+        </ClientProvider>
       </AdminContextProvider>
     </AdminCardProvider>
   );
