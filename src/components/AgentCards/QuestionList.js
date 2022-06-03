0f18b3dce7c3e9c5e2045e0a0b8d816410f3b1fa
Diego Ulibarri <diegoulhe@outlook.com>
2022-06-03 14:15:50 -0500

    Update QuestionList.Js
    - Update Documentation

--- src/components/AgentCards/QuestionList.js
+++ src/components/AgentCards/QuestionList.js
@@ -1,10 +1,21 @@
-import { Fragment, useContext, useState} from "react";
-import { AgentContext } from "../AgentView/AgentProvider";
-// import SearchBar from "./SearchBar";
+/*
+  Authors: Diego Armando Ulibarri Hernández
 
-const QuestionList = (props) => {
-  // Questions where assigned in RowCategoryProblem.js
-  // We access those questions through CardContext
+  Description: Component to show a list with all the questions of a
+  specific problem
+
+  Usage: 
+  <QuestionList />
+*/
+
+import { Fragment, useContext, useState } from "react";
+import { AgentContext } from "../AgentView/AgentProvider";
+
+const QuestionList = () => {
+  // questions -> Contains the questions to be displayed
+  // setSolutions -> Method used to add solutions
+  // setQnA -> Method used to hide the Questions Card
+  // setSolutionCard -> Method used to display the Solutions Card
   const [  ,,,,
     questions,,,,,,,,,,,
     setSolutions,,
@@ -12,91 +23,116 @@ const QuestionList = (props) => {
     setSolutionCard
   ] = useContext(AgentContext);
 
-
+  // Helps us identify the question we are looking for
   const [searchTerm, setSearchTerm] = useState("");
+  // List of questions that are filtered by the searchTerm
   const [searchResult, setSearchResult] = useState([]);
 
+  /**
+   * It takes the search term and filters the questions array
+   * based on the search term.
+   * @param event - the event that triggered the function
+   */
   const searchHandler = async (event) => {
     setSearchTerm(event.target.value);
-    if(searchTerm !== "") {
+    if (searchTerm !== "") {
       const newQuestionList = await questions.filter((question) => {
         return Object.values(question)
           .join(" ")
-          .toLowerCase().includes(searchTerm.toLowerCase());
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
       });
       setSearchResult(newQuestionList);
       //console.log(searchResult);
     } else {
-      setSearchResult(questions)
+      setSearchResult(questions);
     }
   };
 
-
+  /**
+   * It fetches the solution data from the server, then it sets the 
+   * array fo the solutions card with the data and finally it sets the 
+   * state of the solution card to true.
+   * @param id - the id of the problem
+   * @param problem - is the problem description
+   */
   const checkSolution = async (id, problem) => {
-    // const data = {
-    //   solution_id: id
-    // };
-
-    // await fetch("http://localhost:8080/problem/postProblem", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(data),
-    // });
-    // console.log(data);
-
-    const solutionData = await fetch(`http://localhost:8080/problem/getSolutions/${id}`);
+    const solutionData = await fetch(
+      `http://localhost:8080/problem/getSolutions/${id}`
+    );
     const jsonSolution = await solutionData.json();
-    if(jsonSolution.length > 0){
+    if (jsonSolution.length > 0) {
       jsonSolution[0].problem_description = problem;
       setSolutions(jsonSolution);
     } else {
-      setSolutions([{problem_description: problem, problem_id: id}]);
+      setSolutions([{ problem_description: problem, problem_id: id }]);
       //alert("The problem does not have solution");
     }
     const card = document.getElementById("card-6");
     card.display = "block";
     setSolutionCard(true);
-  }
+  };
 
+  /**
+   * When the user clicks the button, the function will hide the card with the id of card-3.
+   */
   const hideQnA = () => {
     const card = document.getElementById("card-3");
     card.display = "none";
     setQnA(false);
-  }
+  };
 
   return (
     <Fragment>
-      <div className="close-btn" onClick={() => hideQnA()}>+</div>
+      <div className="close-btn" onClick={() => hideQnA()}>
+        +
+      </div>
       <p className="title"> Frequent questions </p>
       {/* <SearchBar SearchType="preguntas" /> */}
-      <input type="text" placeholder="search question" value={searchTerm} onChange={searchHandler} />
+      <input
+        type="text"
+        placeholder="search question"
+        value={searchTerm}
+        onChange={searchHandler}
+      />
 
-      {searchTerm.length < 1 ? questions.map(question => {
-        return (
-          <div className="container-question">
-            <p 
-              className="question" 
-              onClick={() => checkSolution(question.problem_id, question.problem_description)}>
-                {question.problem_description}
-            </p>
-          </div>
-        )
-      }) : searchResult.map(question => {
-        return(
-          <div className="container-question">
-            <p 
-              className="question" 
-              onClick={() => checkSolution(question.problem_id, question.problem_description)}>
-                {question.problem_description}
-            </p>
-          </div>
-        )
-      })}
+      {searchTerm.length < 1
+        ? questions.map((question) => {
+            return (
+              <div className="container-question">
+                <p
+                  className="question"
+                  onClick={() =>
+                    checkSolution(
+                      question.problem_id,
+                      question.problem_description
+                    )
+                  }
+                >
+                  {question.problem_description}
+                </p>
+              </div>
+            );
+          })
+        : searchResult.map((question) => {
+            return (
+              <div className="container-question">
+                <p
+                  className="question"
+                  onClick={() =>
+                    checkSolution(
+                      question.problem_id,
+                      question.problem_description
+                    )
+                  }
+                >
+                  {question.problem_description}
+                </p>
+              </div>
+            );
+          })}
     </Fragment>
   );
-
 };
 
 export default QuestionList;
