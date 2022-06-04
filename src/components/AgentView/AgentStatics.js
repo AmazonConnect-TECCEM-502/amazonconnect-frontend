a690de61c9db23459733a195d8002757eca5726c
aenco9 <alex.enriquezco@gmail.com>
2022-06-03 19:13:34 -0500

    add agent dashboard

--- src/components/AgentView/AgentStatics.js
+++ src/components/AgentView/AgentStatics.js
@@ -8,7 +8,15 @@
 import NavBar from "../NavBar/NavBar";
 
 function AgentStatics() {
-  return <NavBar />;
+  return(
+  <div>
+      <NavBar />
+    <iframe
+        width="1200"
+        height="900"
+        src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/559202700801/dashboards/c2baec29-5368-4e6f-be83-fffa16bb7702?directory_alias=amazonconnectbancos#p.agent=7">
+    </iframe>
+  </div>);
 }
 
 export default AgentStatics;
