89563e0c0dbe35ecf19e59cff141508fc8bc82d7
Emilio Rivers <rivers.emilio2001@protonmail.com>
2022-06-09 06:16:20 +0000

    Detection of call start and end

--- src/components/AgentCards/AmazonCCP.js
+++ src/components/AgentCards/AmazonCCP.js
@@ -5,18 +5,69 @@ class AmazonCCP extends Component {
     constructor(props) {
         super(props);
         this.containerDiv = React.createRef();
+	this.onload = true;
     }
 
     componentDidMount() {
+	if(this.onload) {
         connect.core.initCCP(this.containerDiv.current, {
             ccpUrl: "https://tec2022bloqueaws.my.connect.aws/connect/ccp-v2/",
             loginPopup: true,
+		loginPopupAutoClose: true,
             region: "us-east-1",
             softphone: {
                 allowFramedSoftphone: true
             }
         });
-    }
+
+const eventBus = connect.core.getEventBus();
+	    eventBus.subscribe(connect.EventType.TERMINATED, () => {
+				console.log("============================");
+				console.log("============================");
+				  console.log('Logged out');
+				console.log("============================");
+				console.log("============================");
+			});
+
+connect.contact(function (contact) {
+	// Called when the contact is finished (including After Call Work)
+	    contact.onDestroy(function(contact) { 
+				console.log("============================");
+				console.log("============================");
+				  console.log('DESTROYED CONTAAAACT');
+				console.log("============================");
+				console.log("============================");
+	    });
+	// Called both then the call and ACW finish
+	    contact.onEnded(function(contact) { 
+				console.log("============================");
+				console.log("============================");
+				  console.log('ENDED CONTAAAACT');
+				console.log("============================");
+				console.log("============================");
+	    });
+	// Called when a new call starts
+	    contact.onAccepted(function (contact) {
+				console.log("============================");
+				console.log("============================");
+				  console.log('ACCEPTED CONTAAAACT');
+				console.log("============================");
+				console.log("============================");
+
+	    });
+	    contact.onMissed(function (contact) {
+				console.log("============================");
+				console.log("============================");
+				  console.log('MISSED CONTAAAACT');
+				console.log("============================");
+				console.log("============================");
+
+	    });
+});
+
+		this.onload = false;
+	}
+	}
 
     render() {
         return (
