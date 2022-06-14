e31158f725002245e6e01d470668095a24399776
JoanDGG <ggjoan2000@gmail.com>
2022-06-14 17:14:01 -0500

    Update Element.js

--- src/components/Menu/Element.js
+++ src/components/Menu/Element.js
@@ -9,10 +9,10 @@
   <Element element="Problems" elementID="problems"/>
 */
 
-import { useContext/*, useEffect, useState*/ } from "react";
-//import toast from "react-hot-toast";
+import { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { AgentContext } from "../AgentView/AgentProvider";
-//import { ClientContext } from "../ClientCard/ClientProvider";
+import { ClientContext } from "../ClientCard/ClientProvider";
 
 const Element = (props) => {
   const [
@@ -32,14 +32,14 @@ const Element = (props) => {
     setAC,
   ] = useContext(AgentContext);
 
-  /*
+  
   const [, , , , , , , , clientPhone, setClientPhone, , , ,] =
     useContext(ClientContext);
   
   const [showInput, setShowInput] = useState(false);
-  */
+  
   /* Checking if the elementID is equal to client, if it is, then it will set the showInput to true. */
-  /*
+  
   useEffect(() => {
     if (props.elementID === "client") {
       setShowInput(true);
@@ -53,12 +53,12 @@ const Element = (props) => {
    * @param event - The event object is a JavaScript event that is sent to an element when an event
    * occurs on it.
    */
-  /*
+  
   const clientPhoneHandler = (event) => {
     setClientPhone(event.target.value);
   };
-  */
- 
+  
+
   /**
    * If the value of the button clicked is equal to the value of the card, then display the card.
    * @param event - the event that triggered the function
@@ -73,7 +73,6 @@ const Element = (props) => {
       const card = document.getElementById("card-4");
       card.style.display = !client ? "block" : "none";
       setClient(!client);
-      /*
       if(clientPhone !== "")
       {
         setShowInput(!showInput);
@@ -86,7 +85,6 @@ const Element = (props) => {
         toast.error('Please fill in the phone number');
         event.target.checked = false;
       }
-      */
     }
     if (event.target.value === "products") {
       const card = document.getElementById("card-8");
