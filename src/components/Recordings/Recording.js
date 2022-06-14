e906a3b6c4f3def2ced86d60597c5d9eb56fd4ea
JoanDGG <ggjoan2000@gmail.com>
2022-06-14 16:43:34 -0500

    Update Recording.js

--- src/components/Recordings/Recording.js
+++ src/components/Recordings/Recording.js
@@ -148,6 +148,7 @@ const Recording = () => {
             localStorage.removeItem('clientPhone')
             localStorage.setItem('clientPhone', "")
             showContent("not yet");
+            setClientPhone("");
             setInputEmail("");
 
             isCall = false;
