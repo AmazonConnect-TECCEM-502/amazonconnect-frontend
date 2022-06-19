eafec73686247e8b0dc09eaee9ef4072dbb4c580
Emilio Rivers <rivers.emilio2001@protonmail.com>
2022-06-18 21:51:53 -0500

    Update AmazonCCP.js

--- src/components/AgentCards/AmazonCCP.js
+++ src/components/AgentCards/AmazonCCP.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "amazon-connect-streams";
 
+// Class to embed Amazon CCP
 class AmazonCCP extends Component {
     constructor(props) {
         super(props);
@@ -9,22 +10,25 @@ class AmazonCCP extends Component {
         this.isCall = false;
     }
 
+    // This is the equivalet of useEffect, which re-renders the component after change in state
     componentDidMount() {
         if(this.onload) {
+	    // Embeding of Amazon CCP
             connect.core.initCCP(this.containerDiv.current, {
                 ccpUrl: process.env.REACT_APP_CCP_URL,
                 loginPopup: true,
                 loginPopupAutoClose: true,
-                region: "us-east-1",
+                region: "us-east-1", // Region is Virginia
                 softphone: {
                     allowFramedSoftphone: true
                 }
             });
-
+            // Control variable used to lock the rendering to just once
             this.onload = false;
         }
-	}
+    }
 
+    // Function to render the component
     render() {
         return (
             <div>
